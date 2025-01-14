39c89971253383cad71d14989aa6768a90792b32
buttaakhil <159509138+buttaakhil@users.noreply.github.com>
2025-01-14 16:34:19 +0530

    Update TitleComponent.jsx

--- Frontend/src/TitleComponent.jsx
+++ Frontend/src/TitleComponent.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const TitleComponent = () => {
   return (
     <div>
-      <h1 className='text-[#019CCB] text-[44px] sm:text-[56px] font-semibold text-center'>Health Insurance</h1>
-      <p className='text-[#474D66] text-[22px] sm:text-[28px] text-center'>Cost Predictor</p>
+      <h1 className='text-[#019CCB] text-[36px] sm:text-[56px] font-semibold text-center'>Health Insurance</h1>
+      <p className='text-[#474D66] text-[18px] sm:text-[28px] text-center'>Cost Predictor</p>
     </div>
   )
 }
