6af8f498783560c72e825936a9c574f9323716e7
buttaakhil <159509138+buttaakhil@users.noreply.github.com>
2025-04-09 22:37:42 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -32,4 +32,4 @@ def predict(data: Features):
     return {"prediction": prediction.tolist()}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
